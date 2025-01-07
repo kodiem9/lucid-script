47ed31c0898ade85c43a4ac2262540b8eed90fc4
kodiem9 <franczeskostr@gmail.com>
2025-01-07 19:15:39 +0100

    Lucid script lexer #1 task 2

--- example/Main.cpp
+++ example/Main.cpp
@@ -4,6 +4,7 @@ int main() {
     Lucid_Script script("../example/Script.lc");
 
     script.Tokenize();
+    script._TestTokens();
 
     return 0;
 }
--- library/include/Lucid.hpp
+++ library/include/Lucid.hpp
@@ -1,11 +1,18 @@
 #pragma once
-#include <LIB_PCH.hpp>
+#include <LucidData.hpp>
 
 class Lucid_Script {
     public:
         Lucid_Script(const char *file);
         void Tokenize();
+        void _TestTokens();
 
     private:
+        std::vector<Lucid_Token> tokens;
         std::string contents;
+        bool stringQuotation;
+
+        void NewToken(const std::string &name);
+        void NewCharToken(const char &key);
+        bool CharScan(const char &key);
 };
